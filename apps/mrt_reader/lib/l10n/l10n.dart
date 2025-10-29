f274c8b409dc3f763706363fd42c83a6a24a0e30
ishaf <shafiulislam20@gmail.com>
2025-10-29 11:45:31 +0600

    chore(mrt_reader): update iOS configuration and localization
    - Add generated localization files
    - Update iOS project configuration
    - Update melos.yaml configuration

--- apps/mrt_reader/lib/l10n/l10n.dart
+++ apps/mrt_reader/lib/l10n/l10n.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -6,3 +7,4 @@ export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 extension AppLocalizationsX on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this);
 }
+*/
